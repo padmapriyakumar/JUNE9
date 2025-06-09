bab61d50477d81d443577cb1c32073500e8f35d4
padmapriyakumar <padmapriyakumar749@gmail.com>
2025-06-09 15:49:52 +0530

    index

---
+++ Task/python/demo.py
@@ -0,0 +1,42 @@
+"""a= 20
+b=10
+c=5
+if a>b and a>c:
+    print("a is greater")
+elif b>a and b>c:
+    print("b is greater")    
+else:
+    print("c is greater")
+for i in range(1,6,2):
+        print(i)"""
+
+
+
+def add():
+    print(10+20)
+    add()
+        
+list=[1,2,4,5,6,9];
+for i in list:
+     print(i)
+
+
+
+   dictionary={"name":"priya"."password":"47895"}
+   print(dictionary["name"])
+   print(type{dictionary})
+   for key in dictionary:
+       print(dictionary[key])
+
+     
+     
+
+
+
+
+
+
+
+
+
+ 
